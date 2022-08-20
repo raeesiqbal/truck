69dd4683c92c0441e08decc1a305812576fd7a09
mayo <rayiszafar@gmail.com>
2022-08-20 12:30:10 +0500

    dsd

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-j-fhvaoke2tg08^#(1p8ezr5obs-vlnocy7h!(jvsnb1#tfbts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["truck1122.pythonanywhere.com"]
 
 
 # Application definition
@@ -118,12 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 MEDIA_URL = "/images/mysite/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/mysite")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
