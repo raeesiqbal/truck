3d1cf224ebbd37b300ca8431a3ecbeaef50722de
raeesiqbal <rayiszafar@gmail.com>
2022-08-26 15:51:44 +0500

    final changes

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,12 @@
-from multiprocessing import context
 from django.shortcuts import render
-from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic import View
-import datetime
 from tablib import Dataset
 from ortools.linear_solver import pywraplp
-from django.template.loader import get_template
-from xhtml2pdf import pisa
 import xlsxwriter
 import io
+from django.contrib import messages
 
 
 class HomeView(View):
@@ -28,12 +24,13 @@ class FileView(View):
         dataset = Dataset()
         new_resource = request.FILES["file"]
         if not new_resource.name.endswith("xlsx"):
+            messages.error(request, "Please upload a XLSX file.")
             return render(
                 request,
                 "pages/home.html",
             )
         get_data = dataset.load(new_resource.read(), format="xlsx")
-        raees = {}
+        my_dic = {}
         weights = []
         trucks = []
         bundle_numbers = []
@@ -43,15 +40,13 @@ class FileView(View):
             trucks.append(data[0])
             bundle_numbers.append(data[1])
             weights_nw.append(data[3])
-        raees["weights"] = weights
-        raees["items"] = list(range(len(weights)))
-        raees["bins"] = raees["items"]
+        my_dic["weights"] = weights
+        my_dic["items"] = list(range(len(weights)))
+        my_dic["bins"] = my_dic["items"]
         if not request.POST["other_value"]:
-            raees["bin_capacity"] = float(request.POST["bin_capacity"])
-            # print(float(request.POST["bin_capacity"]))
+            my_dic["bin_capacity"] = float(request.POST["bin_capacity"])
         else:
-            # print(float(request.POST["other_value"]))
-            raees["bin_capacity"] = float(request.POST["other_value"])
+            my_dic["bin_capacity"] = float(request.POST["other_value"])
         # Create the mip solver with the SCIP backend.
         solver = pywraplp.Solver.CreateSolver("SCIP")
         if not solver:
@@ -59,51 +54,45 @@ class FileView(View):
         # Variables
         # x[i, j] = 1 if item i is packed in bin j.
         x = {}
-        for i in raees["items"]:
-            for j in raees["bins"]:
+        for i in my_dic["items"]:
+            for j in my_dic["bins"]:
                 x[(i, j)] = solver.IntVar(0, 1, "x_%i_%i" % (i, j))
         # y[j] = 1 if bin j is used.
         y = {}
-        for j in raees["bins"]:
+        for j in my_dic["bins"]:
             y[j] = solver.IntVar(0, 1, "y[%i]" % j)
         # Constraints
         # Each item must be in exactly one bin.
-        for i in raees["items"]:
-            solver.Add(sum(x[i, j] for j in raees["bins"]) == 1)
+        for i in my_dic["items"]:
+            solver.Add(sum(x[i, j] for j in my_dic["bins"]) == 1)
         # The amount packed in each bin cannot exceed its capacity.
-        for j in raees["bins"]:
+        for j in my_dic["bins"]:
             solver.Add(
-                sum(x[(i, j)] * raees["weights"][i] for i in raees["items"])
-                <= y[j] * raees["bin_capacity"]
+                sum(x[(i, j)] * my_dic["weights"][i] for i in my_dic["items"])
+                <= y[j] * my_dic["bin_capacity"]
             )
-
         # Objective: minimize the number of bins used.
-        solver.Minimize(solver.Sum([y[j] for j in raees["bins"]]))
+        solver.Minimize(solver.Sum([y[j] for j in my_dic["bins"]]))
         status = solver.Solve()
-        raees_dic = []
+        my_list = []
         if status == pywraplp.Solver.OPTIMAL:
             num_bins = 0.0
-            for j in raees["bins"]:
+            for j in my_dic["bins"]:
                 if y[j].solution_value() == 1:
                     bin_items = []
                     bin_weight = 0
-                    for i in raees["items"]:
+                    for i in my_dic["items"]:
                         if x[i, j].solution_value() > 0:
                             bin_items.append(i)
-                            bin_weight += raees["weights"][i]
+                            bin_weight += my_dic["weights"][i]
                     if bin_weight > 0:
                         num_bins += 1
                         js = {}
-                        # print("Container number", j)
                         js["num"] = j
-                        # print("  Items packed:", bin_items)
                         js["items"] = bin_items
-                        # print("  Total weight:", bin_weight)
                         js["total"] = bin_weight
-                        raees_dic.append(js)
-                        # print()
-            # print(raees_dic)
-            for i in raees_dic:
+                        my_list.append(js)
+            for i in my_list:
                 com_list = []
                 for ii in i["items"]:
                     com_dict = {}
@@ -117,17 +106,15 @@ class FileView(View):
                     com_dict["gw"] = get_gw
                     com_list.append(com_dict)
                 i["data"] = com_list
-            # print(raees_dic)
         else:
-            context = {"message": "The problem does not have an optimal solution."}
-            # print("The problem does not have an optimal solution.")
-            return render(request, "home.html", context=context)
+            messages.error(request, "The problem does not have an optimal solution.")
+            return render(request, "pages/home.html")
         output = io.BytesIO()
         workbook = xlsxwriter.Workbook(output)
         worksheet = workbook.add_worksheet()
         row = 0
         col = 0
-        for i in raees_dic:
+        for i in my_list:
             worksheet.write(row, col, "container number: {}".format(i["num"]))
             row = row + 2
             worksheet.write(row, col, "Truck")
@@ -153,7 +140,6 @@ class FileView(View):
             worksheet.write(row, col, i["total"])
             col = 0
             row = row + 2
-
         workbook.close()
         output.seek(0)
         filename = "{}.xlsx".format(request.POST["file_name"])
@@ -163,22 +149,3 @@ class FileView(View):
         )
         response["Content-Disposition"] = "attachment; filename=%s" % filename
         return response
-
-        # template_path = "pages/page3.html"
-        # context = {
-        #     "data":raees_dic
-        # }
-        # response = HttpResponse(content_type="application/pdf")
-        # response["Content-Disposition"] = 'attachment; filename="data.pdf"'
-        # template = get_template(template_path)
-        # html = template.render(context)
-
-        # # create a pdf
-        # pisa_status = pisa.CreatePDF(html, dest=response)
-        # # if error then show some funy view
-        # if pisa_status.err:
-        #     return HttpResponse("We had some errors <pre>" + html + "</pre>")
-        # return response
-
-        # context = {"data": raees_dic}
-        # return render(request, "pages/page2.html", context=context)
